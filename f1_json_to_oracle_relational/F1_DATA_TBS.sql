6e943ec40e17b76aa1503d801a7aa2c29a310fc8
Ulf Hellstrom <oraminute@gmail.com>
2019-07-15 17:16:46 +0200

    Fixed bug in view for last race results

--- f1_json_to_oracle_relational/F1_DATA_TBS.sql
+++ f1_json_to_oracle_relational/F1_DATA_TBS.sql
@@ -452,6 +452,7 @@ order by to_number(f1.season),to_number(f1.round),to_number(f1.lap_number),to_nu
 --  DDL for View V_F1_LAST_RACE_RESULTS
 --------------------------------------------------------
 
+
   CREATE OR REPLACE FORCE EDITIONABLE VIEW "F1_DATA"."V_F1_LAST_RACE_RESULTS" ("SEASON", "RACE", "RACENAME", "POSITION", "POINTS", "GIVENNAME", "FAMILYNAME") AS 
   select r.season,
        r.race,
@@ -462,12 +463,12 @@ order by to_number(f1.season),to_number(f1.round),to_number(f1.lap_number),to_nu
        r.familyname
 from v_f1_results r
 where r.season = to_char(trunc(sysdate),'RRRR')
-  and r.race = (select max(race)
+  and r.race = (select max(to_number(race))
                 from v_f1_results
                 where season = to_char(trunc(sysdate),'RRRR'))
   and r.position < 11
-order by to_number(r.position) asc
-;
+order by to_number(r.position) asc;
+
 --------------------------------------------------------
 --  DDL for View V_F1_RACES
 --------------------------------------------------------
