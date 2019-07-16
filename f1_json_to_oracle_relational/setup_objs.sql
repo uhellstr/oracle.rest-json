12c077cab6ce7a4c01bc88f06e001bc075d8bf0d
Ulf Hellstrom <oraminute@gmail.com>
2019-07-16 18:29:38 +0200

    Fixed bug in calculating lap times to milliseconds due to skew data that include hours from ergast

--- f1_json_to_oracle_relational/setup_objs.sql
+++ f1_json_to_oracle_relational/setup_objs.sql
@@ -1,30 +1,41 @@
 @F1_DATA_TBS.sql
 @F1_INIT_PKG.sql
 
+create or replace function  to_millis 
+(
+    p_in_laptime in varchar2
+) return number 
+is
+  v_hour number;
+  v_minutes number;
+  v_seconds number;
+  v_millis  number;
+  lv_retval number;
+
+begin
+
+    if regexp_count(p_in_laptime, ':') = 2 then -- We have hours in the string too 
+      v_hour := to_number(substr(p_in_laptime,1,instr(p_in_laptime,':',1)-1));
+      v_minutes := to_number(substr(p_in_laptime,instr(p_in_laptime,':',1)+1,instr(p_in_laptime,':',2)));
+      v_seconds := to_number(substr(p_in_laptime,instr(p_in_laptime,':',1,2)+1,(length(p_in_laptime) - instr(p_in_laptime,'.',1)-1)));
+      v_millis := to_number(substr(p_in_laptime,instr(p_in_laptime,'.',-1)+1));
+      lv_retval := ((v_hour * 60) * 60000) + (v_minutes * 60000) + (v_seconds * 1000) + v_millis;
+    else -- mi.ss.mi
+      v_minutes := to_number(substr(p_in_laptime,1,instr(p_in_laptime,':',1)-1));
+      v_seconds := to_number(substr(p_in_laptime,instr(p_in_laptime,':',1)+1,(length(p_in_laptime) - instr(p_in_laptime,'.',1)-1)));
+      v_millis  := to_number(substr(p_in_laptime,instr(p_in_laptime,'.',-1)+1));
+      lv_retval := (v_minutes * 60000) + (v_seconds * 1000) + v_millis;
+    end if;
+    return  lv_retval;
+
+end to_millis;
+/
+
 --exec dbms_scheduler.drop_schedule('SCH_F1_LOAD_DATA');
 --exec dbms_scheduler.drop_job('JOB_RUN_F1_INIT_PKG', false);
 --exec dbms_scheduler.drop_chain('CHAIN_1', false);
 --exec dbms_scheduler.drop_program('F1_INIT_DATA', false);
   
-create or replace function f1_data.to_millis 
-(
-    p_in_laptime in varchar2
-) return number 
-is
-  v_minutes number;
-  v_seconds number;
-  v_millis  number;
-  
-begin
-
-    v_minutes := to_number(substr(p_in_laptime,1,instr(p_in_laptime,':',1)-1));
-    v_seconds := to_number(substr(p_in_laptime,instr(p_in_laptime,':',1)+1,(length(p_in_laptime) - instr(p_in_laptime,'.',1)-1)));
-    v_millis  := to_number(substr(p_in_laptime,instr(p_in_laptime,'.',-1)+1));
-    return  (v_minutes * 60000) + (v_seconds * 1000) + v_millis;
-    
-end to_millis;
-/
-
 BEGIN
 
   --exec dbms_scheduler.drop_schedule('SCH_F1_LOAD_DATA');
