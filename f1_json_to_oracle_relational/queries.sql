b6198029c9841f576ce989dd1ac6eb7e47a7f0e3
Ulf Hellstrom <oraminute@gmail.com>
2020-01-02 17:14:19 +0100

    Restructed and fixes in example query script

--- f1_json_to_oracle_relational/queries.sql
+++ f1_json_to_oracle_relational/queries.sql
@@ -1,23 +1,19 @@
--- Give us all world champions in Formula 1!!
+-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-- This is a testsuite of different queries against the F1 database
+--                   Run these queries as F1_ACCESS
+-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+-- Give us all world champions in Formula 1!!
 select
     d.season,
-    d.race,
-    d.position,
-    d.positiontext,
-    d.points,
-    d.wins,
-    d.driverid,
-    d.permanentnumber,
-    d.code,
-    d.info,
     d.givenname,
     d.familyname,
-    d.dateofbirth,
-    d.nationality,
-    d.constructorid,
-    d.constructorinfo,
+    d.nationality,    
+    d.points,
+    d.wins,
+    d.info,
     d.constructorname,
+    d.constructorinfo,
     d.constructornationality
 from
     f1_data.v_f1_driverstandings d
@@ -42,9 +38,7 @@ from
 order by d.season desc;
 
 -- Give us the number of championships a champ has got! E.g who is the ultimate champ!
-
-select driverid,
-       givenname,
+select givenname,
        familyname,
        nationality,
        championships_won
@@ -99,17 +93,13 @@ from
 ) group by driverid,givenname,familyname,nationality
 ) order by championships_won desc;
 
--- Give us the constructor champiions over the years
-
+-- Give us the constructor champions over the years
 select
   c.season,
-  c.race,
-  c.position,
   c.points,
   c.wins,
-  c.constructorid,
-  c.constructorinfo,
   c.constructorname,
+  c.constructorinfo,
   c.constructornationality
 from
   f1_data.v_f1_constructorstandings c
@@ -133,16 +123,64 @@ where to_number(c.race) = (select max(to_number(d.round))
                         ))
 order by to_number(c.season) desc;
 
+-- Which constructore has most championships over all ?
+select *
+from
+(
+select
+  count(c.season) as championship_wins,
+  c.constructorname,
+  c.constructorinfo,
+  c.constructornationality
+from
+  f1_data.v_f1_constructorstandings c
+where to_number(c.race) = (select max(to_number(d.round))
+                           from f1_data.v_f1_races d
+                           where to_number(d.season) = to_number(c.season))
+  and to_number(c.position) = 1
+  and c.season <= (select season -- Is current season finished yet?
+                       from
+                       (
+                         select to_date(r.race_date,'RRRR-MM-DD') as race_date
+                                ,case 
+                                   when r.race_date < trunc(sysdate) then to_char(trunc(sysdate),'RRRR')
+                                   when r.race_date > trunc(sysdate) then to_char(to_number(to_char(trunc(sysdate),'RRRR'))-1)
+                                   else '1900'
+                                 end as season
+                         from f1_data.v_f1_seasons_race_dates r
+                         where r.season = c.season
+                           and to_number(r.round) in (select max(to_number(rd.round)) from f1_data.v_f1_seasons_race_dates rd
+                                                      where rd.season  = r.season)
+                        ))
+group by   c.constructorid,
+           c.constructorinfo,
+           c.constructorname,
+           c.constructornationality 
+) order by to_number(championship_wins) desc;
+
+-- Between what years did "Hunt the shunt" race in Formula 1 ?
+select dri.givenname
+       ,dri.familyname
+       ,dri.nationality
+       ,min(re.season) as starting_year
+       ,max(re.season) as last_year
+from f1_data.v_f1_drivers dri
+inner join f1_data.mv_f1_results re
+  on dri.driverid = re.driverid
+where dri.driverid = 'hunt'
+group by dri.givenname
+         ,dri.familyname
+         ,dri.nationality;
+
 --- Show all ME9 Ericssons F1 races during his career --
 select *
-from f1_data.v_f1_results
+from f1_data.mv_f1_results
 where pilotnr = 9
   and season >= 2014
 order by to_number(season),to_number(race) asc;
 
 
 -- List all times Alonso has driven at Monza
-
 select season,
        race,
        locality,
@@ -158,13 +196,12 @@ select season,
        status,
        ranking,
        fastestlap       
-from f1_data.v_f1_results
+from f1_data.mv_f1_results
 where locality = 'Monza'
   and driverid = 'alonso'
 order by to_number(season),to_number(race);
 
 -- What is the fastest laptime set on Monza on the current layout on the track from 2000 and forward ?
-
 select 
     l.season,
     l.round,
@@ -195,6 +232,42 @@ from f1_data.v_f1_drivers
 where upper(nationality) = 'SWEDISH'
 order by to_date(dateofbirth,'YYYY-MM-DD') desc;
 
+-- Show us the "active" years for swedish f1 drivers and there best result and total number of races in there carier.
+select  givenname
+       ,familyname
+       ,nationality
+       ,starting_year
+       ,last_year
+       ,case 
+          when last_year - starting_year > 0 then (last_year - starting_year) +1
+          else 1
+        end as active_years ,
+        best_position_in_race,
+        total_number_of_races
+from
+(
+with nat_drivers as
+( select /*+ MATERIALIZE */ nat.driverid 
+  from f1_data.v_f1_drivers nat
+  where upper(nat.nationality) = 'SWEDISH'
+)
+select dri.givenname
+       ,dri.familyname
+       ,dri.nationality
+       ,min(to_number(re.season)) as starting_year
+       ,max(to_number(re.season)) as last_year
+       ,min(to_number(re.position)) as best_position_in_race
+       ,count(re.race) as total_number_of_races
+from f1_data.v_f1_drivers dri
+inner join f1_data.mv_f1_results re
+  on dri.driverid = re.driverid
+where dri.driverid in ( select nat.driverid from nat_drivers nat
+                        where dri.driverid = nat.driverid)
+group by dri.givenname
+         ,dri.familyname
+         ,dri.nationality
+) order by starting_year desc;
+
 -- List all races ME9 Ericsson scored points during his F1 time.
 select eri.season,
        eri.race,
@@ -217,7 +290,6 @@ where eri.driverid = 'ericsson'
 order by to_number(eri.season),to_number(eri.race);
 
 -- How many points in total did ME9 Ericsson get during his F1 career.
-
 select sum(eri.points) as totalcareerpoints
 from f1_data.v_f1_results eri
 where eri.driverid = 'ericsson'
@@ -231,7 +303,6 @@ where alo.driverid = 'alonso'
   and alo.points > 0;
   
 -- Get the median position for ME9 Ericsson during his F1 career
-
 select r.driverid,
        median(to_number(r.position)) as median_position
 from f1_data.v_f1_results r
@@ -239,7 +310,6 @@ where r.driverid = 'ericsson'
 group by r.driverid;
 
 -- Get median posititon for all drivers in the modern era 2010-2019
-
 select driverid,
        givenname,
        familyname,
@@ -259,67 +329,6 @@ group by r.driverid,r.givenname,r.familyname
 ) where number_of_races > 10
 order by median_position asc,number_of_races desc;
 
--- Get the median score for drivers between 2000-2010
-select driverid,
-       givenname,
-       familyname,
-       median_position,
-       number_of_races
-from
-(
-select r.driverid,
-       r.givenname,
-       r.familyname,
-       median(to_number(r.position)) as median_position,
-       (select count(b.driverid) from v_f1_results b
-        where b.driverid = r.driverid) as number_of_races
-from f1_data.v_f1_results r
-where to_number(r.season) between 2000 and 2010              
-group by r.driverid,r.givenname,r.familyname
-) where number_of_races > 10
-order by median_position asc,number_of_races desc;
-
--- Get the median score for drivers late 90's
-
-select driverid,
-       givenname,
-       familyname,
-       median_position,
-       number_of_races
-from
-(
-select r.driverid,
-       r.givenname,
-       r.familyname,
-       median(to_number(r.position)) as median_position,
-       (select count(b.driverid) from v_f1_results b
-        where b.driverid = r.driverid) as number_of_races
-from f1_data.v_f1_results r
-where to_number(r.season) between 1990 and 2000              
-group by r.driverid,r.givenname,r.familyname
-) where number_of_races > 10
-order by median_position asc,number_of_races desc;
-
---And the median score of drivers during the 80's era..
-select driverid,
-       givenname,
-       familyname,
-       median_position,
-       number_of_races
-from
-(
-select r.driverid,
-       r.givenname,
-       r.familyname,
-       median(to_number(r.position)) as median_position,
-       (select count(b.driverid) from v_f1_results b
-        where b.driverid = r.driverid) as number_of_races
-from f1_data.v_f1_results r
-where to_number(r.season) between 1980 and 1990              
-group by r.driverid,r.givenname,r.familyname
-) where number_of_races > 10
-order by median_position asc,number_of_races desc;
-
 -- Get the number of races that ME9 partisipated in F1.
 select count(*) 
 from f1_data.v_f1_results
@@ -332,7 +341,7 @@ from
 select r.givenname
        ,r.familyname
        ,count(r.race) as total_races
-from f1_data.v_f1_results r
+from f1_data.mv_f1_results r
 where r.nationality = 'Swedish'
 group by r.givenname
          ,r.familyname
@@ -344,30 +353,12 @@ select a.season,
        a.race_date
 from f1_data.v_f1_seasons_race_dates a
 where a.round not in ( select b.race
-                     from f1_data.v_f1_results b
+                     from f1_data.mv_f1_results b
                      where a.season = b.season
                        and a.round = b.race)
   and a.season = to_char(trunc(sysdate),'RRRR');
   
--- Get the score points for the last race
-
-select r.season,
-       r.race,
-       r.racename,
-       r.position,
-       r.points,
-       r.givenname,
-       r.familyname
-from f1_data.v_f1_results r
-where r.season = to_char(trunc(sysdate),'RRRR')
-  and to_number(r.race) = (select max(to_number(race))
-                           from f1_data.v_f1_results
-                           where season = to_char(trunc(sysdate),'RRRR'))
-  and r.position < 11
-order by to_number(r.position) asc;
-
 -- Give us the qualification times from 1994 and forward (Full data from 2002 ca)
-
 select
   q.season,
   q.round,
@@ -427,7 +418,6 @@ group by q.season
 -- Give us all polesitters and number of polepositions thru the history since about 2000 and forward
 -- Note: Ergast dont give us all data so we only see absolute correct data from 2003 and forward
 -- Means that data for Senna as an example is completly messed up and only shows 3 polepositions
-
 select *
 from
 (
@@ -480,7 +470,7 @@ where q.constructor = (select distinct(q1.constructor)
                        from f1_data.mv_f1_qualification_times q1
                        where q1.season = 2018
                          and q1.constructor = q.constructor)
-  and q.season = 2018
+  and to_number(q.season) = 2018
 ) where internal_position = 1
 group by season
        ,drivernumber
@@ -490,7 +480,6 @@ group by season
 ) order by season,constructorname,outqualify_teammate;
 
 -- Get the starting grid for the latest race in current season
-
 select
   season,
   round,
@@ -538,7 +527,6 @@ where to_number(season) = to_number(to_char(trunc(sysdate),'RRRR'))
 order by to_number(position) asc;
 
 -- Give us the fastest qualification lap on a track
-
 select season,
        round,
        racedate,
@@ -615,8 +603,7 @@ where lp.season = 2019
                             where lp1.season = 2019
                               and lp1.round = 9);
 
--- How many times has Hamilton won his homerace at Silverstone ?
-
+-- Show us Hamilton's race wins and Silverstone uk.
 select
   f1r.season,
   f1r.race,
@@ -640,7 +627,6 @@ from
 order  by season desc;
 
 -- Give us the winners at Silverstonde thru history and the total number of wins.
-
 select circuitname,
        locality,
        country,
@@ -703,7 +689,6 @@ from f1_data.v_f1_results r
 where lower(r.status) = 'fatal accident';
 
 -- Give us the accidents that Ericsson was involved in race situation.
-
 select r.*
 from f1_data.v_f1_results r
 where lower(r.status) = 'accident'
