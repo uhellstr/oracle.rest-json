74750d1a58e9e0480e54659163a9c26c4a68fa90
Ulf Hellström <oraminute@gmail.com>
2022-04-14 10:19:14 +0200

    Added new helper function for getting the last race faster calculation

--- f1_json_to_oracle_relational/sql/setup_objs.sql
+++ f1_json_to_oracle_relational/sql/setup_objs.sql
@@ -116,4 +116,35 @@ end get_check_season;
 
 grant execute on f1_logik.get_check_season to f1_access;
 
+create or replace function get_last_race 
+(
+  p_in_cur_year in varchar2 default to_char(current_date,'rrrr') 
+) return number result_cache 
+as 
+
+  lv_retval number;
+  
+begin
+
+    with last_race as -- we need to check if any upcoming races or if the last race for the season is done.
+    (
+    select /*+ MATERIALIZE */  nvl(min(to_number(x.round)-1),-1) as race -- check if any upcoming races this seaseon -1 and season is done
+    from f1_data.v_f1_upcoming_races x
+    where x.season = p_in_cur_year
+    )
+    select case when race = -1 then (select max(to_number(y.round))
+                                   from  f1_data.v_f1_races y
+                                   where y.season = to_char(to_number(p_in_cur_year)-1))
+          else race
+          end race
+          into lv_retval
+          from last_race;
+          
+   return lv_retval;
+
+end get_last_race;
+/
+
+grant execute on f1_logik.get_last_race to f1_access;
+
 @F1_DATA_SCHEDULER.sql
